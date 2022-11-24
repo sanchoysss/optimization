51ec53b74050221f270cfe6cce9532da75565b29
almi1221 <name@email.comaliaksandr.miranovich@netcracker.com>
2022-11-25 02:17:20 +0300

    Initial commit

---
+++ quote-service/src/test/java/by/alex/quote/QuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.alex.quote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
