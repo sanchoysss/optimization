12277cc98485dc4034734faafd8ce6dbef63ed47
almi1221 <name@email.comaliaksandr.miranovich@netcracker.com>
2022-11-27 17:09:30 +0300

    Bpo service added

---
+++ bpo-service/src/main/java/by/alex/bpo/BpoServiceApplication.java
@@ -0,0 +1,13 @@
+package by.alex.bpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpoServiceApplication.class, args);
+	}
+
+}
---
+++ bpo-service/src/test/java/by/alex/bpo/BpoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.alex.bpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
