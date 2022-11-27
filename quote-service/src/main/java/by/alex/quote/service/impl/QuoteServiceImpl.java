c10fd96d747f6791b24bb14d84df7463b0ca8b5b
almi1221 <name@email.comaliaksandr.miranovich@netcracker.com>
2022-11-27 17:06:34 +0300

    Logging added

--- quote-service/src/main/java/by/alex/quote/service/impl/QuoteServiceImpl.java
+++ quote-service/src/main/java/by/alex/quote/service/impl/QuoteServiceImpl.java
@@ -4,16 +4,20 @@ import by.alex.quote.model.Quote;
 import by.alex.quote.repository.QuoteRepository;
 import by.alex.quote.service.QuoteService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class QuoteServiceImpl implements QuoteService {
 
     private QuoteRepository quoteRepository;
 
     @Override
     public Quote createQuote(Quote quote) {
-        return quoteRepository.save(quote);
+        Quote createdQuote = quoteRepository.save(quote);
+        log.info("createQuote: created quote with id {}", quote.getId());
+        return createdQuote;
     }
 }
